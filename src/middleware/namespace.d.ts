f09555503016177fdf860163d2d0bde981f8ea66
SerKo <44749100+serkodev@users.noreply.github.com>
2022-10-13 05:32:42 +0000

    refactor: declare module in ts

--- src/middleware/namespace.d.ts
+++
@@ -1,7 +0,0 @@
-import '@tsndr/cloudflare-worker-router'
-
-declare module '@tsndr/cloudflare-worker-router' {
-  interface RouterRequest {
-    namespace: KVNamespace
-  }
-}
--- src/middleware/namespace.ts
+++ src/middleware/namespace.ts
@@ -1,5 +1,11 @@
 import type { RouterHandler } from '@tsndr/cloudflare-worker-router'
 
+declare module '@tsndr/cloudflare-worker-router' {
+  interface RouterRequest {
+    namespace: KVNamespace
+  }
+}
+
 const namespaceMiddleware: RouterHandler = ({ req, env, next }) => {
   if (req.params.namespace_identifier) {
     const namespaceIdentifier = decodeURIComponent(req.params.namespace_identifier)
