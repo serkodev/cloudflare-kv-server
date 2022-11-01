fd91f1830725aaf04a19c51dfe97e8ce084fbfa9
SerKo <44749100+serkodev@users.noreply.github.com>
2022-11-01 10:49:32 +0000

    styles: remove MasterCSS JIT

--- packages/auth-token-gen/src/main.ts
+++ packages/auth-token-gen/src/main.ts
@@ -1,10 +1,5 @@
 import { createApp } from 'vue'
-import MasterCSS from '@master/css'
-// @ts-expect-error "js"
-import config from '../master.css.js'
 import '@master/normal.css'
 import App from './App.vue'
-
-export const css = new MasterCSS({ config })
 
 createApp(App).mount('#app')
